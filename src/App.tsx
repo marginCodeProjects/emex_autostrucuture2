d41df289c22e568db1b435ad1f7e3fcbac9547ca
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2024-08-19 17:34:32 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Settings from "./pages/Settings/Settings";
 import Main from "./pages/Main/Main";
@@ -11,35 +11,33 @@ import Login from "./pages/Login/Login";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   const handleLoginSuccess = (isSuccess: boolean) => {
     setIsAuthenticated(isSuccess);
   };
+
   useEffect(() => {
-    // Проверяем наличие cookie при загрузке компонента
     const token = getCookie('access_token');
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-  }, [isAuthenticated]);
+    setIsAuthenticated(!!token);
+  }, []);
 
   return (
     <LanguageProvider>
       <Router>
         <div>
-          {isAuthenticated ? (
-            <>
-              <Header onLogoutSuccess={handleLoginSuccess} />
-              <Routes>
+          {isAuthenticated && <Header onLogoutSuccess={handleLoginSuccess} />}
+          <Routes>
+            <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+            {isAuthenticated ? (
+              <>
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<Settings />} />
                 <Route path="/contact" element={<History />} />
-              </Routes>
-            </>
-          ) : (
-            <Login onLoginSuccess={handleLoginSuccess} />
-          )}
+              </>
+            ) : (
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            )}
+          </Routes>
         </div>
       </Router>
     </LanguageProvider>
