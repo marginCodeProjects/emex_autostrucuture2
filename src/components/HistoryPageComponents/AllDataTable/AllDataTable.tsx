2618e63a49427910c4cc1c888a13c2b3c16da6e8
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2025-03-11 12:50:52 +0300

    current size

--- src/components/HistoryPageComponents/AllDataTable/AllDataTable.tsx
+++ src/components/HistoryPageComponents/AllDataTable/AllDataTable.tsx
@@ -200,8 +200,8 @@ const AllDataTable: React.FC<AllDataTableProps> = ({ setFileName, setFileType })
                             pageSize: lineOnPage, // Количество строк на странице по умолчанию
                             showSizeChanger: true, // Позволяет менять количество строк на странице
                             pageSizeOptions: ['5', '10', '20'], // Доступные варианты количества строк на странице
-                            onShowSizeChange: (size) => {
-                                setLineOnPage(size)
+                            onShowSizeChange: (current) => {
+                                setLineOnPage(current)
                             }
                         }}
                     />
